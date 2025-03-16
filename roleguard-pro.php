a7421fe33dd57e50597cca71d37ac20e7f505d41
Sagor Rana <sagorranait@gmail.com>
2025-03-16 09:51:00 +0600

    New plugin is coming to live.

---
+++ roleguard-pro.php
@@ -0,0 +1,20 @@
+<?php
+ defined( 'ABSPATH' ) || exit;
+
+/**
+ * Plugin Name: RoleGuard Pro
+ * Description: RoleGuard Pro is a WordPress plugin that enhances security by allowing you to create custom user roles with specific capabilities. You can add an "Admin" role with limited access to plugins and sensitive features, ensuring security while delegating tasks effectively. It's ideal for multi-user environments.
+ * Plugin URI: https://github.com/sagorranait/roleguard-pro
+ * Author: Sagor Rana
+ * Author URI: https://github.com/sagorranait/
+ * Version: 1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/sagorranait/quizify/releases
+ *
+ * Text Domain: roleguard
+ * Domain Path: /languages
+ *
+ */
