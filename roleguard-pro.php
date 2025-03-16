0649ad583ef042e55911a3e4cbdfbbda63e0f32a
Sagor Rana <sagorranait@gmail.com>
2025-03-16 09:59:50 +0600

    Basic setup is done.

--- roleguard-pro.php
+++ roleguard-pro.php
@@ -18,3 +18,28 @@
  * Domain Path: /languages
  *
  */
+
+ final class Roleguard {
+    static function version() {return '1.0.0';}
+    static function author_name() {return 'Sagor Rana';}
+    static function min_php_version() {return '7.0';}
+    static function plugin_file() {return __FILE__;}
+    static function plugin_url() {return trailingslashit(plugin_dir_url(__FILE__));}
+    static function plugin_dir() {return trailingslashit(plugin_dir_path(__FILE__));}
+
+    public function __construct() {
+		add_action( 'init', array( $this, 'i18n' ) );	
+		add_action( 'plugins_loaded', array( $this, 'run' ));
+	}
+
+    public function i18n(){load_plugin_textdomain('roleguard', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/');}
+    public static function install_activation_hook(){flush_rewrite_rules();}
+
+    public function run() {
+       echo "Sagor Rana New plugin";
+    }
+ }
+
+ new Roleguard();
+
+ register_activation_hook( __FILE__, 'Roleguard::install_activation_hook' );
